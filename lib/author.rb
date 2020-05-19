fbd7c0a32413640505126b7a640d3fa2470912c9
Uriah Harston <uharston@gmail.com>
2020-05-19 18:38:52 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # class Author
 #
 # attr_accessor :name, :posts
@@ -62,3 +63,69 @@ class Author
     # self.songs.collect {|n| n.name}
   end
 end
+=======
+class Author
+
+attr_accessor :name, :posts
+
+ @@post_count = 0
+
+ def initialize(name)
+   @name = name
+   @posts = []
+ end
+
+ def add_post(post)
+   @posts << post
+   post.author = self
+   @@post_count += 1
+ end
+
+ def add_post_by_title(title)
+   post = Post.new(title)
+   @posts << post
+   post.author = self
+   @@post_count += 1
+ end
+
+ def self.post_count
+   @@post_count
+ end
+end
+
+# class Author
+#
+#   attr_accessor :name, :posts
+#
+#   @@post_count = 0
+#
+#   def initialize(name)
+#     @name = name
+#     @posts = []
+#   end
+#
+#   def add_post(post)
+#     @posts << post
+#     post.author = self
+#     @@post_count += 1
+#   end
+#
+#   def add_post_by_title(title)
+#     post = Post.new(title)
+#     add_post(post)
+#     @posts << post
+#     post.author = self
+#     @@post_count += 1
+#   end
+#
+#   def self.post_count
+#     @@post_count
+#   end
+#
+#   def post
+#     @post
+#     # Post.all.select {|n| n.title == self}
+#     # self.songs.collect {|n| n.name}
+#   end
+# end
+>>>>>>> d2f0a2235dbe2bc24e9623a95aae1df9dcbf3f0b
--- lib/post.rb
+++ lib/post.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 class Post
@@ -8,6 +9,13 @@ class Post
   def initialize(title)
     @title = title
     @@all << self
+=======
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+>>>>>>> d2f0a2235dbe2bc24e9623a95aae1df9dcbf3f0b
   end
 
   def author_name
@@ -17,8 +25,35 @@ class Post
       @author.name
     end
   end
+<<<<<<< HEAD
 
   def self.all
     @@all
   end
 end
+=======
+end
+
+# class Post
+#
+#   @@all = []
+#
+#   attr_accessor :title, :author
+#   def initialize(title)
+#     @title = title
+#     @@all << self
+#   end
+#
+#   def author_name
+#     if @author == nil
+#       return nil
+#     else
+#       @author.name
+#     end
+#   end
+#
+#   def self.all
+#     @@all
+#   end
+# end
+>>>>>>> d2f0a2235dbe2bc24e9623a95aae1df9dcbf3f0b
